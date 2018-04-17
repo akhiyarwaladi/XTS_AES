45a2dfcfe45b7c243ac85dd8aa9dfe186dc21f86
akhiyarwaladi <akiyar18@gmail.com>
2018-04-17 12:32:26 +0700

    haha

--- src/xts_aes/Decryption.java
+++ src/xts_aes/Decryption.java
@@ -5,6 +5,11 @@
  */
 package xts_aes;
 
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author proxc
@@ -14,6 +19,8 @@ public class Decryption extends javax.swing.JFrame {
     /**
      * Creates new form Home_Data
      */
+    String sourceField, keyField, targetField;
+    String sourceName, keyName, targetName;
     public Decryption() {
         initComponents();
     }
@@ -31,11 +38,11 @@ public class Decryption extends javax.swing.JFrame {
         jLabel15 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        cipherFileTF = new javax.swing.JTextField();
+        keyFileTF = new javax.swing.JTextField();
+        keyFileButton = new javax.swing.JButton();
+        cipherFileButton = new javax.swing.JButton();
+        decryptButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -69,26 +76,36 @@ public class Decryption extends javax.swing.JFrame {
 
         jLabel2.setText("Key File");
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        cipherFileTF.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                cipherFileTFActionPerformed(evt);
             }
         });
 
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+        keyFileTF.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
+                keyFileTFActionPerformed(evt);
             }
         });
 
-        jButton1.setText("Browse");
-
-        jButton2.setText("Browse");
-
-        jButton3.setText("Decrypt");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        keyFileButton.setText("Browse");
+        keyFileButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                keyFileButtonActionPerformed(evt);
+            }
+        });
+
+        cipherFileButton.setText("Browse");
+        cipherFileButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cipherFileButtonActionPerformed(evt);
+            }
+        });
+
+        decryptButton.setText("Decrypt");
+        decryptButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                decryptButtonActionPerformed(evt);
             }
         });
 
@@ -104,15 +121,15 @@ public class Decryption extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jLabel1)
                             .addComponent(jLabel2)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
-                            .addComponent(jTextField1))
+                            .addComponent(keyFileTF, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                            .addComponent(cipherFileTF))
                         .addGap(40, 40, 40)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1)
-                            .addComponent(jButton2)))
+                            .addComponent(keyFileButton)
+                            .addComponent(cipherFileButton)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(300, 300, 300)
-                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(decryptButton, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -123,33 +140,89 @@ public class Decryption extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(cipherFileTF, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cipherFileButton, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(51, 51, 51)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 39, Short.MAX_VALUE))
+                    .addComponent(keyFileButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(keyFileTF, javax.swing.GroupLayout.DEFAULT_SIZE, 39, Short.MAX_VALUE))
                 .addGap(61, 61, 61)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(decryptButton, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 80, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void cipherFileTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cipherFileTFActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_cipherFileTFActionPerformed
 
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+    private void keyFileTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_keyFileTFActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
+    }//GEN-LAST:event_keyFileTFActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void decryptButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_decryptButtonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+        
+        System.out.println("Decryption Start!");
+        
+        sourceName = sourceField;
+        keyName = keyField;
+        
+        try {
+            //String basename = FilenameUtils.getBaseName(sourceField)+"_decrypted";
+            
+            targetName = sourceField.substring(0, sourceField.lastIndexOf("."));
+            targetName = targetName.concat(".decrypted");
+            
+            System.out.println("Saved in "+targetName);
+            
+            
+            // Use relative path for Unix systems
+            File f = new File(targetName);
+            f.getParentFile().mkdirs(); 
+            f.createNewFile();
+            
+            XTS_AES aes = new XTS_AES(sourceName, keyName, targetName);
+            aes.startDecryption(targetName, keyName, sourceName);
+            
+        }catch(Exception ex) {
+            Logger.getLogger(Decrypt.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_decryptButtonActionPerformed
+
+    private void cipherFileButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cipherFileButtonActionPerformed
+        // TODO add your handling code here:
+        JFileChooser jfc = new JFileChooser(new File("D:\\"));
+
+        int returnValue = jfc.showOpenDialog(null);
+        // int returnValue = jfc.showSaveDialog(null);
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = jfc.getSelectedFile();
+            System.out.println(selectedFile.getAbsolutePath());
+            this.cipherFileTF.setText(selectedFile.getAbsolutePath());
+            sourceField = selectedFile.getAbsolutePath();
+        }
+    }//GEN-LAST:event_cipherFileButtonActionPerformed
+
+    private void keyFileButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_keyFileButtonActionPerformed
+        // TODO add your handling code here:
+        JFileChooser jfc = new JFileChooser(new File("D:\\"));
+
+        int returnValue = jfc.showOpenDialog(null);
+        // int returnValue = jfc.showSaveDialog(null);
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = jfc.getSelectedFile();
+            System.out.println(selectedFile.getAbsolutePath());
+            this.keyFileTF.setText(selectedFile.getAbsolutePath());
+            keyField = selectedFile.getAbsolutePath();
+        }
+    }//GEN-LAST:event_keyFileButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -188,14 +261,14 @@ public class Decryption extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JButton cipherFileButton;
+    private javax.swing.JTextField cipherFileTF;
+    private javax.swing.JButton decryptButton;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JButton keyFileButton;
+    private javax.swing.JTextField keyFileTF;
     // End of variables declaration//GEN-END:variables
 }
